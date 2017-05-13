38db992340bd54edcfec553f6172dfa8c19885ab
Happy Dzayer <happy05@programmer.net>
2017-05-13 22:31:51 +0100

    Updates

---
+++ Batch Art/TV no signal color bars.bat
@@ -0,0 +1,32 @@
+::TV no signal color bars
+@echo OFF
+mode con cols=80 lines=25
+color f
+cls
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[44m           [40m           [45m           [43m           [46m           [40m           [47m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[41m              [47m             [44m             [47m               [46m           [42m           
+echo.[47m                       [40m           NO SIGNAL           [47m                       
+echo.[43m                             [41m                   [43m                             
+pause >nul
+
+
--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Collection of Batch scripts, examples and some details for various commands are considered. The subjects include system administration, file management, computer maintenance,  Internet tools, and network administration. 
 
+<div align="center">
+  <img src="https://raw.githubusercontent.com/happy05dz/Batch-Script-Collection/master/_images/banner.gif" width="auto" height="auto" />
+  </div>
+  
+  
 ---
 ## License
 [![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
